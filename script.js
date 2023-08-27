69dfaa70f751666b0c5dbe11825ea65c1deace0e
Mohit0120 <moh.sharma.2901@gmail.com>
2023-08-27 21:23:46 +0530

    second Commit

---
+++ script.js
@@ -0,0 +1,122 @@
+var timeout;
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+function firstPageAnim(){
+    let tl = gsap.timeline();
+    tl.from("#nav",{
+        y: '-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+    .to('.boundingelem',{
+        y:'0',
+        delay: -1,
+        duration: 2,
+        ease: Expo.easeInOut,
+        stagger: .2
+    })
+    .from('#herofooter',{
+        y:'10',
+        opacity: 0,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut
+
+    })
+}
+
+function skewCircle(){
+    //define default scale value
+        var xscale=1;
+        var yscale=1;
+
+        var xprev = 0;
+        var yprev = 0;
+
+
+    window.addEventListener("mousemove",function(dets){
+        clearTimeout(timeout);
+        // var xdiff = dets.clientX-xprev;
+        // var ydiff = dets.clientY-yprev;
+
+        xscale = gsap.utils.clamp(0.8,1.2,dets.clientX-xprev);
+        yscale = gsap.utils.clamp(0.8,1.2,dets.clientY-yprev);
+
+
+        xprev = dets.clientX;
+        yprev = dets.clientY;
+
+        circleMouseFollower(xscale, yscale);
+
+        timeout = setTimeout(function(){
+            document.querySelector('#minicircle').style.transform = `translate(${dets.clientX}px ,${dets.clientY}px) scale(1, 1)`
+        },100)
+
+    })
+}
+
+function circleMouseFollower(xscale, yscale){
+    window.addEventListener("mousemove", function(details){
+        document.querySelector('#minicircle').style.transform = `translate(${details.clientX}px ,${details.clientY}px) scale(${xscale}, ${yscale})`
+    })
+}
+
+skewCircle();
+circleMouseFollower();
+firstPageAnim();
+
+document.querySelectorAll(".elem").forEach(function(elem){
+    var rotate = 0;
+    var diffrot = 0;
+
+    elem.addEventListener("mouseleave",function(details){
+        document.querySelector(
+            "#minicircle"
+          ).style.width = `10px`;
+          document.querySelector(
+            "#minicircle"
+          ).style.height = `10px`;
+          document.querySelector(
+            "#minicircle"
+          ).style.opacity = `1`;
+          document.querySelector(
+            "#minicircle"
+          ).textContent = ``;
+        gsap.to(elem.querySelector("img"), {
+            opacity:0,
+            ease: Power3,
+            duration: 0.5,
+        })
+    })
+
+    elem.addEventListener("mousemove",function(details){
+        document.querySelector(
+            "#minicircle"
+          ).style.width = `100px`;
+          document.querySelector(
+            "#minicircle"
+          ).style.height = `100px`;
+          document.querySelector(
+            "#minicircle"
+          ).style.opacity = `0.5`;
+          document.querySelector(
+            "#minicircle"
+          ).textContent = `view`;
+        var diff = details.clientY-elem.getBoundingClientRect().top;
+        diffrot = details.clientX - rotate;
+        rotate = details.clientX;
+        gsap.to(elem.querySelector("img"), {
+            opacity:1,
+            ease: Power3,
+            top: diff,
+            left: details.clientX,
+            rotate: gsap.utils.clamp(-20, 20, diffrot*0.5),
+        })
+    })
+})
+
